25769396147cae80da58d59e3ef0aec8e153089d
Yua Akimoto <>
2019-04-21 14:30:01 +0900

    resolve conflict

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float speed = 4.0f;
+    float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
